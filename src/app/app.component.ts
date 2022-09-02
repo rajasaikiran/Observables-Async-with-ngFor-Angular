1e5afa527d278b070794308c35f90e3e2f499bca
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-09-02 15:15:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BulkdataService } from './bulkdata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'async-pipe';
+
+  public serviceData!: Observable<any[]>;
+
+    // using subscribe
+// public serviceData: any = [];
+
+  constructor(private bulkData: BulkdataService) {}
+
+  ngOnInit(): void {
+    this.getEveryData();
+  }
+
+ private getEveryData() {
+    this.serviceData = this.bulkData.getAllData();
+    console.log(this.serviceData);
+
+    // using subscribe
+    // this.bulkData.getAllData().subscribe((data)=>{
+    //   this.serviceData = data;
+    //   console.log(this.serviceData)
+    // })
+  }
 }
